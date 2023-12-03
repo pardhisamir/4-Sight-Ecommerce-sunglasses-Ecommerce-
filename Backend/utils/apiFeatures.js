716cd0cf9026862e6a8d0083f265f07e08752fe8
pardhisamir <87870533+pardhisamir@users.noreply.github.com>
2023-12-03 18:22:16 +0530

    Add files via upload

---
+++ Backend/utils/apiFeatures.js
@@ -0,0 +1,43 @@
+class ApiFeatures {
+    constructor(query, queryStr) {
+        this.query = query;
+        this.queryStr = queryStr;
+    }
+
+    search() {
+        const keyword = this.queryStr.keyword ? {
+            name: {
+                $regex: this.queryStr.keyword,
+                $options: "i", // case insensitive
+            }
+        } : {};
+
+        this.query.find({ ...keyword });
+        return this;
+    }
+
+    filter() {
+        const queryCopy = { ...this.queryStr };
+
+        // Removing some fields for category
+        const removeFields = ["keyword", "page", "limit"];
+        removeFields.forEach(key => delete removeFields[key]);
+
+        // Filter for price and rating
+        let queryStr = JSON.stringify(queryCopy);
+        queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, (key) => `$${key}`);
+
+        // final query
+        this.query = this.query.find(JSON.parse(queryStr));
+        return this;
+    }
+
+    pagination(resultsPerPage) {
+        const currentPage = Number(this.queryStr.page) || 1;
+        const skip = resultsPerPage * (currentPage - 1);
+        this.query = this.query.limit(resultsPerPage).skip(skip);
+        return this;
+    }
+}
+
+module.exports = ApiFeatures;
